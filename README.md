4ce6fd233deadb46f43030755e474d1769eb21f9
paydak <103631646+paydakk@users.noreply.github.com>
2024-01-11 09:13:43 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# woxz
+# mustifixwashere.vercel.app
+
+This open source project is my active website built using React, Vite and Tailwindcss.
+
+> Designer and coding: @mustifix
---
+++ src/config.ts
@@ -0,0 +1,44 @@
+import { BsDiscord, BsInstagram, BsSteam, BsGithub, Bs123, BsYoutube, BsTwitch } from 'react-icons/bs';
+import { IoLogoJavascript, IoLogoCss3 } from 'react-icons/io';
+import { AiFillHtml5 } from 'react-icons/ai'
+import { SiTypescript, SiVite } from 'react-icons/si';
+import { DiNodejsSmall, DiReact, DiVisualstudio } from 'react-icons/di';
+import { TbBrandNextjs } from 'react-icons/tb';
+
+export default {
+    'github': {
+        'username': 'Woxz'
+    },
+    'discord': {
+        'id': '961348197568106516'
+    },
+    'social_media_accounts': [
+        {
+            'label': 'Discord Users',
+            'to': 'https://discord.com/users/961348197568106516',
+            'icon': BsDiscord,
+        },
+        {
+            'label': 'Discord Server',
+            'to': 'https://discord.gg/walorant',
+            'icon': BsDiscord,
+        },
+        {
+            'label': 'İnstagram',
+            'to': 'https://instagram.com/berkaysz7',
+            'icon': BsDiscord,
+        },
+
+        ],
+    'techs': [
+        {
+            'lang': 'Twitch',
+            'icon': BsTwitch
+        },
+        {
+            'lang': 'YouTube',
+            'icon': BsYoutube
+        },
+    ]
+}
+
